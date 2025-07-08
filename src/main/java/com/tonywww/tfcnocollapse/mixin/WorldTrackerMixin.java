4eba73088573497f8372a34af38ee0f20a8ad9bd
Tonywww <1204484415@qq.com>
2025-07-07 18:43:01 -0700

    让阻止塌方的逻辑变得更加激进

--- src/main/java/com/tonywww/tfcnocollapse/mixin/WorldTrackerMixin.java
+++ src/main/java/com/tonywww/tfcnocollapse/mixin/WorldTrackerMixin.java
@@ -1,7 +1,8 @@
 package com.tonywww.tfcnocollapse.mixin;
 
-import com.tonywww.tfcnocollapse.TFCNoCollapse;
+import net.dries007.tfc.util.collections.BufferedList;
 import net.dries007.tfc.util.tracker.Collapse;
+import net.dries007.tfc.util.tracker.TickEntry;
 import net.dries007.tfc.util.tracker.WorldTracker;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
@@ -14,6 +15,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Collection;
+import java.util.List;
 
 import static com.tonywww.tfcnocollapse.TFCNoCollapse.DIM_SET;
 
@@ -25,12 +27,25 @@ public abstract class WorldTrackerMixin {
     private Level level;
 
 
+    @Shadow(remap = false)
+    @Final
+    private List<Collapse> collapsesInProgress;
+
+    @Shadow(remap = false)
+    @Final
+    private BufferedList<BlockPos> isolatedPositions;
+
+    @Shadow(remap = false)
+    @Final
+    private BufferedList<TickEntry> landslideTicks;
+
     @Inject(at = @At("HEAD"), method = "addLandslidePos(Lnet/minecraft/core/BlockPos;)V", cancellable = true, remap = false)
     public void addLandslidePos(BlockPos centerPos, CallbackInfo ci) {
 //        TFCNoCollapse.getLogger().debug("NOCOLDEBUG: " + DIM_SET + this.level.dimension().location());
         if (DIM_SET == null) return;
         ResourceLocation dim = this.level.dimension().location();
         if (!DIM_SET.contains(dim)) {
+            landslideTicks.clear();
             ci.cancel();
         }
     }
@@ -41,6 +56,7 @@ public abstract class WorldTrackerMixin {
         if (DIM_SET == null) return;
         ResourceLocation dim = this.level.dimension().location();
         if (!DIM_SET.contains(dim)) {
+            isolatedPositions.clear();
             ci.cancel();
         }
     }
@@ -51,6 +67,7 @@ public abstract class WorldTrackerMixin {
         if (DIM_SET == null) return;
         ResourceLocation dim = this.level.dimension().location();
         if (!DIM_SET.contains(dim)) {
+            collapsesInProgress.clear();
             ci.cancel();
         }
     }
@@ -61,6 +78,7 @@ public abstract class WorldTrackerMixin {
         if (DIM_SET == null) return;
         ResourceLocation dim = this.level.dimension().location();
         if (!DIM_SET.contains(dim)) {
+            collapsesInProgress.clear();
             ci.cancel();
         }
     }
